516e9a05f00e2ca73a059a49e4b75ac179a1853a
Jerome Habanz <jerome_jero@t-online.de>
2024-01-12 11:16:03 +0100

    optimized dataset filtering

--- utils.py
+++ utils.py
@@ -1,6 +1,4 @@
 from matplotlib import pyplot as plt
-from torch.utils.data import Subset
-from tqdm import tqdm
 
 
 def plot_keypoints(kpts):
@@ -10,45 +8,26 @@ def plot_keypoints(kpts):
             if v >= 0.9:
                 plt.scatter(x, y, color='blue', marker='o', s=1)
 
-# TODO: Integrate this function in CocoWholeBOdy using the ids
-def _coco_remove_images_without_annotations(dataset):
+
+def _has_valid_annotation(anno, min_keypoints_per_image):
     def _has_only_empty_bbox(anno):
-        return all(any(o <= 1 for o in obj[2:]) for obj in anno["boxes"])
+        return all(any(o <= 1 for o in obj["bbox"][2:]) for obj in anno)
 
     def _count_visible_keypoints(anno):
-        return sum(sum(1 for x, y, v in ann if v > 0) for ann in anno["keypoints"])
+        return sum(sum(1 for v in ann["keypoints"][2::3] if v > 0) +
+                   sum(1 for v in ann["foot_kpts"][2::3] if v > 0) +
+                   sum(1 for v in ann["face_kpts"][2::3] if v > 0) +
+                   sum(1 for v in ann["lefthand_kpts"][2::3] if v > 0) +
+                   sum(1 for v in ann["righthand_kpts"][2::3] if v > 0) for ann in anno)
 
-    # TODO: Think about changing this proportionally to NUM_KEYPOINTS
-    min_keypoints_per_image = 50
-
-    def _has_valid_annotation(anno):
-        # if it's empty, there is no annotation
-        if len(anno) == 0:
-            return False
-        # if all boxes have close to zero area, there is no annotation
-        if _has_only_empty_bbox(anno):
-            return False
-        # keypoints task have a slight different critera for considering
-        # if an annotation is valid
-        # if "keypoints" not in anno:
-        #     return True
-        # for keypoint detection tasks, only consider valid images those
-        # containing at least min_keypoints_per_image
-        if _count_visible_keypoints(anno) >= min_keypoints_per_image:
-            return True
+    # if it's empty, there is no annotation
+    if len(anno) == 0:
         return False
-
-    print("Removing targets without annotations...")
-    status_data = tqdm(enumerate(dataset),
-                       desc="Processing",
-                       total=len(dataset),
-                       unit="targets",
-                       mininterval=60)
-    ids = []
-    for i, (image, targets) in status_data:
-        if _has_valid_annotation(targets):
-            ids.append(i)
-
-    dataset = Subset(dataset, ids)
-    print(f"{len(dataset)} targets remain")
-    return dataset
+    # if all boxes have close to zero area, there is no annotation
+    if _has_only_empty_bbox(anno):
+        return False
+    # for keypoint detection tasks, only consider valid images those
+    # containing at least min_keypoints_per_image
+    if _count_visible_keypoints(anno) >= min_keypoints_per_image:
+        return True
+    return False
