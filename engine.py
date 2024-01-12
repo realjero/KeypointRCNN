792a3cc2a5ba8150d35b396b438ae11eadfb0c47
Jerome Habanz <jerome_jero@t-online.de>
2024-01-12 09:40:01 +0100

    tweaking hyperparams

--- engine.py
+++ engine.py
@@ -4,15 +4,14 @@ import sys
 from torch.optim.lr_scheduler import LinearLR
 
 
-def train_one_epoch(model, optimizer, data_loader, device, epoch, status_bar, print_freq=10):
+def train_one_epoch(model, optimizer, data_loader, device, epoch, status_bar, print_freq=10, sched=None):
     model.train()
 
-    lr_schedular = None
     if epoch == 0:
         warmup_factor = 1.0 / 1000
         warmup_iters = min(1000, len(data_loader) - 1)
 
-        lr_scheduler = LinearLR(optimizer, start_factor=warmup_factor, total_iters=warmup_iters)
+        sched = LinearLR(optimizer, start_factor=warmup_factor, total_iters=warmup_iters)
 
     # TODO: Create Intercept to log metrics
     for images, targets in data_loader:
@@ -31,5 +30,5 @@ def train_one_epoch(model, optimizer, data_loader, device, epoch, status_bar, pr
         optimizer.step()
         status_bar.set_description(f"Loss: {losses}")
 
-        if lr_scheduler is not None:
-            lr_scheduler.step()
+        if sched is not None:
+            sched.step()
